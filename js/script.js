5dcad827584e48fc0469685ebc81a275e22d5ae8
Abdallah Askar <abdoasker.1426@gmail.com>
2022-07-30 10:51:32 +0200

    implementing a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -47,6 +47,24 @@ allLinks.forEach(function(link){
       
   })
 })
+///////////////////////////////////////////////////
+// stricky navigation
+///////////////////////////////////////////////////
+const sectionHeroEl = document.querySelector(".section-hero")
+const obs =new IntersectionObserver(function(entries){
+  const ent = entries[0]
+  if (ent.isIntersecting === false)
+      document.body.classList.add('sticky');
+  else
+      document.body.classList.remove('sticky');
+},
+{
+  root:null,
+  threshold:0,
+  rootMargin:'-80px'
+
+});
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 ///////////////////////////////////////////////////////////
